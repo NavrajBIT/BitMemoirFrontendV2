844b8bb88f17f7dd587a5a850b3e5fe1b84b0c6d
MaahiVohra <vohramaahi@gmail.com>
2023-09-12 14:10:35 +0530

    added subscription details

--- components/dashboard/subcomponents/heading.jsx
+++ components/dashboard/subcomponents/heading.jsx
@@ -5,97 +5,110 @@ import Image from "next/image";
 import { useState } from "react";
 
 const Heading = ({ usedash }) => {
-  const [isMenu, setIsMenu] = useState(false);
-  const userDetails = {
-    name: `${
-      usedash.accountDetails.first_name
-        ? usedash.accountDetails.first_name
-        : "Anonymous"
-    } ${usedash.accountDetails.last_name && usedash.accountDetails.last_name}`,
-    email: usedash.emailDetails.email && usedash.emailDetails.email,
-    phone: usedash.accountDetails.phone && usedash.accountDetails.phone,
-    wallet: usedash.accountDetails.wallet && usedash.accountDetails.wallet,
-  };
-  return (
-    <div className={style.sectionContainer}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div style={{ fontSize: "1.5rem", color: "var(--text-bright)" }}>
-          {userDetails.name}
-          {usedash.kycDetails.status !== "Approved" && (
-            <span style={{ fontSize: "0.75rem", color: "red" }}>
-              KYC pending*
-            </span>
-          )}
-        </div>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <div>Email: {userDetails.email}</div>
-          <div>Phone: {userDetails.phone}</div>
-          <div>Wallet: {userDetails.wallet}</div>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
-          gap: "var(--padding-light)",
-          flexWrap: "wrap",
-        }}
-      >
-        <div
-          className={style.dotmenu}
-          onClick={() => setIsMenu((prev) => !prev)}
-          style={{
-            background: isMenu ? "var(--primary-80)" : "var(--primary-90)",
-          }}
-        >
-          {!isMenu ? (
-            <Image
-              src={"/icons/dot-menu.svg"}
-              width={30}
-              height={50}
-              alt="menu"
-            />
-          ) : (
-            "X"
-          )}
-        </div>
-        {isMenu && <Menu />}
-      </div>
-    </div>
-  );
+	const [isMenu, setIsMenu] = useState(false);
+	const userDetails = {
+		name: `${
+			usedash.accountDetails.first_name
+				? usedash.accountDetails.first_name
+				: "Anonymous"
+		} ${
+			usedash.accountDetails.last_name && usedash.accountDetails.last_name
+		}`,
+		email: usedash.emailDetails.email && usedash.emailDetails.email,
+		phone: usedash.accountDetails.phone && usedash.accountDetails.phone,
+		wallet: usedash.accountDetails.wallet && usedash.accountDetails.wallet,
+	};
+	const subscriptionDetails = {
+		plan:
+			usedash.subscriptionDetails.plan &&
+			usedash.subscriptionDetails.plan,
+		nft_quota:
+			usedash.subscriptionDetails.nft_quota &&
+			usedash.subscriptionDetails.nft_quota,
+	};
+	return (
+		<div className={style.sectionContainer}>
+			<div style={{ display: "flex", flexDirection: "column" }}>
+				<div
+					style={{ fontSize: "1.5rem", color: "var(--text-bright)" }}>
+					{userDetails.name}
+					{usedash.kycDetails.status !== "Approved" && (
+						<span style={{ fontSize: "0.75rem", color: "red" }}>
+							KYC pending*
+						</span>
+					)}
+				</div>
+				<div style={{ display: "flex", flexDirection: "column" }}>
+					<div>Email: {userDetails.email}</div>
+					<div>Phone: {userDetails.phone}</div>
+					<div>Wallet: {userDetails.wallet}</div>
+					<div>Subscription Plan: {subscriptionDetails.plan}</div>
+					<div>NFT's Left: {subscriptionDetails.nft_quota}</div>
+				</div>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-around",
+					gap: "var(--padding-light)",
+					flexWrap: "wrap",
+				}}>
+				<div
+					className={style.dotmenu}
+					onClick={() => setIsMenu((prev) => !prev)}
+					style={{
+						background: isMenu
+							? "var(--primary-80)"
+							: "var(--primary-90)",
+					}}>
+					{!isMenu ? (
+						<Image
+							src={"/icons/dot-menu.svg"}
+							width={30}
+							height={50}
+							alt="menu"
+						/>
+					) : (
+						"X"
+					)}
+				</div>
+				{isMenu && <Menu />}
+			</div>
+		</div>
+	);
 };
 
 export default Heading;
 
 const Menu = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div className={style.menuContainer}>
-      <Button
-        text="Logout"
-        variant={"secondary"}
-        onClick={() => {
-          localStorage.setItem("jwtToken", null);
-          router.push("/home");
-        }}
-      />
+	return (
+		<div className={style.menuContainer}>
+			<Button
+				text="Logout"
+				variant={"secondary"}
+				onClick={() => {
+					localStorage.setItem("jwtToken", null);
+					router.push("/home");
+				}}
+			/>
 
-      <Button
-        text="Edit Profile"
-        variant={"secondary"}
-        onClick={() => {
-          router.push("/kyc");
-        }}
-      />
-      <Button
-        text="KYC Status"
-        variant={"secondary"}
-        onClick={() => {
-          router.push("/kyc/status");
-        }}
-      />
-    </div>
-  );
+			<Button
+				text="Edit Profile"
+				variant={"secondary"}
+				onClick={() => {
+					router.push("/kyc");
+				}}
+			/>
+			<Button
+				text="KYC Status"
+				variant={"secondary"}
+				onClick={() => {
+					router.push("/kyc/status");
+				}}
+			/>
+		</div>
+	);
 };
--- components/dashboard/usedashboard.js
+++ components/dashboard/usedashboard.js
@@ -2,143 +2,159 @@ import { useState, useEffect } from "react";
 import API from "../subcomponents/scripts/apiCall";
 
 const usedashboard = () => {
-  const api = API();
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedTab, setSelectedTab] = useState("Profile");
-  const [accountDetails, setAccountDetails] = useState(null);
-  const [kycDetails, setKycDetails] = useState(null);
-  const [emailDetails, setEmailDetails] = useState(null);
-  const [organizationDetails, setOrganizationDetails] = useState(null);
-  const [issuerDetails, setIssuerDetails] = useState(null);
-  const [approversList, setApproversList] = useState(null);
-  const [orders, setOrders] = useState(null);
-  const [certificates, setCertificates] = useState(null);
+	const api = API();
+	const [isLoading, setIsLoading] = useState(false);
+	const [selectedTab, setSelectedTab] = useState("Profile");
+	const [accountDetails, setAccountDetails] = useState(null);
+	const [subscriptionDetails, setSubscriptionDetails] = useState(null);
+	const [kycDetails, setKycDetails] = useState(null);
+	const [emailDetails, setEmailDetails] = useState(null);
+	const [organizationDetails, setOrganizationDetails] = useState(null);
+	const [issuerDetails, setIssuerDetails] = useState(null);
+	const [approversList, setApproversList] = useState(null);
+	const [orders, setOrders] = useState(null);
+	const [certificates, setCertificates] = useState(null);
 
-  useEffect(() => {
-    poppulateData();
-  }, []);
+	useEffect(() => {
+		poppulateData();
+	}, []);
 
-  const poppulateData = async () => {
-    setIsLoading(true);
-    await poppulateAccountDetails();
-    await poppulateEmailDetails();
-    await poppulateOrganizationDetails();
-    await poppulateIssuerDetails();
-    await poppulateApprovers();
-    await poppulateOrders();
-    await poppulateCertificates();
-    await poppulateKYCDetails();
-    setIsLoading(false);
-  };
+	const poppulateData = async () => {
+		setIsLoading(true);
+		await poppulateAccountDetails();
+		await poppulateEmailDetails();
+		await poppulateOrganizationDetails();
+		await poppulateIssuerDetails();
+		await poppulateApprovers();
+		await poppulateOrders();
+		await poppulateCertificates();
+		await poppulateKYCDetails();
+		await poppulateSubscriptionDetails();
+		setIsLoading(false);
+	};
 
-  const poppulateAccountDetails = async () => {
-    await api
-      .crud("GET", "user/account")
-      .then((res) => {
-        console.log(res[0]);
-        if (res.status === 200) {
-          setAccountDetails(res[0]);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-  const poppulateKYCDetails = async () => {
-    await api
-      .crud("GET", "user/kyc")
-      .then((res) => {
-        console.log(res[0]);
-        if (res.status === 200) {
-          setKycDetails(res[0]);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-  const poppulateEmailDetails = async () => {
-    await api
-      .crud("GET", "user/email")
-      .then((res) => {
-        console.log(res[0]);
-        if (res.status === 200) {
-          setEmailDetails(res[0]);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-  const poppulateOrganizationDetails = async () => {
-    await api
-      .crud("GET", "user/organization")
-      .then((res) => {
-        console.log(res[0]);
-        if (res.status === 200) {
-          setOrganizationDetails(res[0]);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-  const poppulateIssuerDetails = async () => {
-    await api
-      .crud("GET", "user/issuer")
-      .then((res) => {
-        console.log(res[0]);
-        if (res.status === 200) {
-          setIssuerDetails(res[0]);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-  const poppulateApprovers = async () => {
-    await api
-      .crud("GET", "user/approver")
-      .then((res) => {
-        console.log(res);
-        if (res.status >= 200 && res.status <= 299) setApproversList(res);
-      })
-      .catch((er) => console.log(er));
-  };
-  const poppulateOrders = async () => {
-    await api
-      .crud("GET", "certificate/order")
-      .then((res) => {
-        console.log(res);
-        if (res.status >= 200 && res.status <= 299) setOrders(res);
-      })
-      .catch((er) => console.log(er));
-  };
-  const poppulateCertificates = async () => {
-    await api
-      .crud("GET", "certificate/all")
-      .then((res) => {
-        console.log(res);
-        if (res.status >= 200 && res.status <= 299) setCertificates(res);
-      })
-      .catch((er) => console.log(er));
-  };
+	const poppulateAccountDetails = async () => {
+		await api
+			.crud("GET", "user/account")
+			.then((res) => {
+				console.log(res[0]);
+				if (res.status === 200) {
+					setAccountDetails(res[0]);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateSubscriptionDetails = async () => {
+		await api
+			.crud("GET", "subscription/subscription-detail")
+			.then((res) => {
+				console.log(res);
+				if (res.status === 200) {
+					setSubscriptionDetails(res);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateKYCDetails = async () => {
+		await api
+			.crud("GET", "user/kyc")
+			.then((res) => {
+				console.log(res[0]);
+				if (res.status === 200) {
+					setKycDetails(res[0]);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateEmailDetails = async () => {
+		await api
+			.crud("GET", "user/email")
+			.then((res) => {
+				console.log(res[0]);
+				if (res.status === 200) {
+					setEmailDetails(res[0]);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateOrganizationDetails = async () => {
+		await api
+			.crud("GET", "user/organization")
+			.then((res) => {
+				console.log(res[0]);
+				if (res.status === 200) {
+					setOrganizationDetails(res[0]);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateIssuerDetails = async () => {
+		await api
+			.crud("GET", "user/issuer")
+			.then((res) => {
+				console.log(res[0]);
+				if (res.status === 200) {
+					setIssuerDetails(res[0]);
+				}
+			})
+			.catch((err) => console.log(err));
+	};
+	const poppulateApprovers = async () => {
+		await api
+			.crud("GET", "user/approver")
+			.then((res) => {
+				console.log(res);
+				if (res.status >= 200 && res.status <= 299)
+					setApproversList(res);
+			})
+			.catch((er) => console.log(er));
+	};
+	const poppulateOrders = async () => {
+		await api
+			.crud("GET", "certificate/order")
+			.then((res) => {
+				console.log(res);
+				if (res.status >= 200 && res.status <= 299) setOrders(res);
+			})
+			.catch((er) => console.log(er));
+	};
+	const poppulateCertificates = async () => {
+		await api
+			.crud("GET", "certificate/all")
+			.then((res) => {
+				console.log(res);
+				if (res.status >= 200 && res.status <= 299)
+					setCertificates(res);
+			})
+			.catch((er) => console.log(er));
+	};
 
-  const deleteApprover = async (approver) => {
-    setIsLoading(true);
-    console.log(approver);
-    await api
-      .crud("DELETE", `user/approver/${approver.id}`)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-    setIsLoading(false);
-    poppulateApprovers();
-  };
+	const deleteApprover = async (approver) => {
+		setIsLoading(true);
+		console.log(approver);
+		await api
+			.crud("DELETE", `user/approver/${approver.id}`)
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+		setIsLoading(false);
+		poppulateApprovers();
+	};
 
-  return {
-    isLoading,
-    accountDetails,
-    kycDetails,
-    emailDetails,
-    organizationDetails,
-    issuerDetails,
-    approversList,
-    deleteApprover,
-    selectedTab,
-    setSelectedTab,
-    orders,
-    certificates,
-  };
+	return {
+		isLoading,
+		accountDetails,
+		subscriptionDetails,
+		kycDetails,
+		emailDetails,
+		organizationDetails,
+		issuerDetails,
+		approversList,
+		deleteApprover,
+		selectedTab,
+		setSelectedTab,
+		orders,
+		certificates,
+	};
 };
 
 export default usedashboard;
