4ccdc4c909ec1be6728962c286ec4c6f11133bba
bishtn05 <87189178+bishtn05@users.noreply.github.com>
2023-11-24 12:56:30 +0530

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,20 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import GoogleAnalytics from "@/components/subcomponents/scripts/googleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bitmemoir: Verified Certificates over Blockchain",
-  description: "Transforming the education system",
+  title: "Bitmemoir: Verified Certificates and Documents over Blockchain",
+  description: "Issue. Store. Verify. Retrieve.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics
+        GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
